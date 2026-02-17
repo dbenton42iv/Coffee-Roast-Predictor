dd6a2d2a6458a44841a8f494600913ec2cb3f1a7
David Benton <dbenton@carolina.rr.com>
2026-02-17 14:05:03 -0500

    Create ControlPanel.js for React component

---
+++ frontend/src/components/ControlPanel.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ControlPanel = () => {
+    return (
+        <div className="control-panel">
+            <h2>Control Panel</h2>
+            {/* Add your controls here */}
+        </div>
+    );
+};
+
+export default ControlPanel;
